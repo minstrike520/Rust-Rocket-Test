f548fb1e7abaacac32fb926435cfee10473d5836
minstrike520 <123291118+minstrike520@users.noreply.github.com>
2023-05-23 11:50:31 +0000

    D4: Dynamic and static GET handler

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 #[macro_use] extern crate rocket;
 
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/demo")]
+fn get_demo() -> &'static str {
+    "abc"
+}
+
+#[get("/hello/<name>/<age>/<cool>")]
+fn hello(name: &str, age: u8, cool: bool) -> String {
+    format!("{name},{age},{cool}")
+}
+
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index,get_demo,hello])
 }
